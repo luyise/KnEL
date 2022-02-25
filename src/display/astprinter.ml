5a28cbb0039ffc8a6477e1fd09a77a7cd25fa349
Louise L <louise.leclerc37@gmail.com>
2022-02-25 22:54:21 +0100

    improved color handling in pretty printer

--- src/display/astprinter.ml
+++ src/display/astprinter.ml
@@ -1,47 +1,108 @@
 open Ast
 
-let pp_ident fmt ident = Format.fprintf fmt "%s" ident
+type coloration =
+  | CLR_elm   (* couleur des éléments, tels que →, Π, λ, Σ, , ...*)
+  | CLR_par   (* couleur des parenthèses *)
+  | CLR_cst   (* couleur des constantes, telles que Type, ⊥, Unit... *)
+  | CLR_var   (* couleur des variables liées *)
+  | CLR_nam  (* couleur des noms définis par l'utilisateurs : noms de théorèmes, définition, hypothèses etc *)
+  | CLR_def   (* couleur par défaut *)
+
+let ansi_escape_code clr = match clr with
+  | CLR_elm -> "\x1B[38;5;179m"     (* orange, pâle *)
+  | CLR_par -> "\x1B[38;5;242m"     (* gris, terne *)
+  | CLR_cst -> "\x1B[38;5;181m"     (* ocre, pâle *)
+  | CLR_var -> "\x1B[38;5;109m"     (* vert, pâle *)
+  | CLR_nam -> "\x1B[38;5;183m"     (* lavande, pâle *)
+  | CLR_def -> "\x1B[39m"      (* gris, très pâle *)
+
+let pp_ident clr fmt ident =
+  let pref = ansi_escape_code clr in
+  let suff = ansi_escape_code CLR_def in
+  Format.fprintf fmt "%s%s%s" pref ident suff
 
 let rec pp_expr fmt (exp : expr) =
   match exp with
     | EConst id -> Format.fprintf fmt "%a"
-        pp_ident id
+        (pp_ident CLR_cst)
+        (match id with
+          | "Void" -> "⊥"
+          | _ -> id
+        )
     | EVar id -> Format.fprintf fmt "%a"
-        pp_ident id
-    | ELam ((id , exp1) , exp2) -> Format.fprintf fmt "(\x1B[38;5;130mλ\x1B[39m (%a \x1B[38;5;130m:\x1B[39m %a) \x1B[38;5;130m→\x1B[39m %a)"
-        pp_ident id
+        (pp_ident CLR_var) id
+    | ELam ((id , exp1) , exp2) -> Format.fprintf fmt "%a%a %a%a %a %a%a %a %a%a" (* "(λ (%a : %a) → %a)" *)
+        (pp_ident CLR_par) "("
+        (pp_ident CLR_elm) "λ"
+        (pp_ident CLR_par) "("
+        (pp_ident CLR_var) id
+        (pp_ident CLR_elm) ":"
         pp_expr exp1
+        (pp_ident CLR_par) ")"
+        (pp_ident CLR_elm) "→"
         pp_expr exp2
-    | EApp (exp1 , exp2) -> Format.fprintf fmt "(%a %a)"
+        (pp_ident CLR_par) ")"
+    | EApp (exp1 , exp2) -> Format.fprintf fmt "%a%a %a%a" (* "(%a %a)" *)
+        (pp_ident CLR_par) "("
         pp_expr exp1
+        (pp_ident CLR_par) ")"
         pp_expr exp2
     | EPi ((id , exp1) , exp2) ->
-        if id = "_" then
-          Format.fprintf fmt "(%a \x1B[38;5;130m→\x1B[39m %a)"
+        if id = "_" then              (* "(%a → %a)" *)
+          Format.fprintf fmt "%a%a %a %a%a"
+          (pp_ident CLR_par) "("
           pp_expr exp1
+          (pp_ident CLR_elm) "→" 
           pp_expr exp2
-        else
-          Format.fprintf fmt "(\x1B[38;5;130mΠ\x1B[39m (%a \x1B[38;5;130m:\x1B[39m %a)\x1B[38;5;130m,\x1B[39m %a)"
-          pp_ident id
+          (pp_ident CLR_par) ")"
+        else                          (* "(Π (%a : %a), %a)"*)
+          Format.fprintf fmt "%a%a %a%a %a %a%a%a %a%a"
+          (pp_ident CLR_par) "("
+          (pp_ident CLR_elm) "Π"
+          (pp_ident CLR_par) "("
+          (pp_ident CLR_var) id
+          (pp_ident CLR_elm) ":"
           pp_expr exp1
+          (pp_ident CLR_par) ")"
+          (pp_ident CLR_elm) ","
           pp_expr exp2
-    | EPair ((exp1 , exp2) , _) -> Format.fprintf fmt "(%a \x1B[38;5;130m,\x1B[39m %a)"
+          (pp_ident CLR_par) ")"
+    | EPair ((exp1 , exp2) , _) -> Format.fprintf fmt "%a%a %a %a%a"    (* "(%a , %a)" *)
+        (pp_ident CLR_par) "("
         pp_expr exp1
+        (pp_ident CLR_elm) ","
         pp_expr exp2
-    | EFst exp1 -> Format.fprintf fmt "(\x1B[38;5;130mfst\x1B[39m %a)"
+        (pp_ident CLR_par) ")"
+    | EFst exp1 -> Format.fprintf fmt "%a%a %a%a"       (* "(fst %a)" *)
+        (pp_ident CLR_par) "("
+        (pp_ident CLR_cst) "fst"
         pp_expr exp1
-    | ESnd exp1 -> Format.fprintf fmt "(\x1B[38;5;130msnd\x1B[39m %a)"
+        (pp_ident CLR_par) ")"
+    | ESnd exp1 -> Format.fprintf fmt "%a%a %a%a"       (* "(snd %a)" *)
+        (pp_ident CLR_par) "("
+        (pp_ident CLR_cst) "snd"
         pp_expr exp1
+        (pp_ident CLR_par) ")"
     | ESigma ((id , exp1) , exp2) -> 
-        if id = "_" then
-          Format.fprintf fmt "(%a \x1B[38;5;130m×\x1B[39m %a)"
+        if id = "_" then              (* "(%a × %a)" *)
+          Format.fprintf fmt "%a%a %a %a%a"
+          (pp_ident CLR_par) "("
           pp_expr exp1
+          (pp_ident CLR_elm) "×" 
           pp_expr exp2
-        else
-          Format.fprintf fmt "(\x1B[38;5;130mΣ\x1B[39m (%a \x1B[38;5;130m:\x1B[39m %a)\x1B[38;5;130m,\x1B[39m %a)"
-          pp_ident id
+          (pp_ident CLR_par) ")"
+        else                          (* "(Σ (%a : %a), %a)"*)
+          Format.fprintf fmt "%a%a %a%a %a %a%a%a %a%a"
+          (pp_ident CLR_par) "("
+          (pp_ident CLR_elm) "Σ"
+          (pp_ident CLR_par) "("
+          (pp_ident CLR_var) id
+          (pp_ident CLR_elm) ":"
           pp_expr exp1
+          (pp_ident CLR_par) ")"
+          (pp_ident CLR_elm) ","
           pp_expr exp2
+          (pp_ident CLR_par) ")"
     | ETaggedExpr (exp1 , _) -> Format.fprintf fmt "%a"
         pp_expr exp1
 
@@ -55,8 +116,8 @@ let pp_context fmt (ctx : context) =
           pp_expr s)
     else
       (fun (name, s) ->
-        Format.fprintf fmt "\t%s : %a\n" 
-          name
+        Format.fprintf fmt "\t%a : %a\n" 
+          (pp_ident CLR_var) name
           pp_expr s)
   in
   List.iter f ctx
