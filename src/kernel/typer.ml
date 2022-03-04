28e4bf521119837238775b3c5fc0af3fad0a214f
Louise L <louise.leclerc37@gmail.com>
2022-03-04 22:54:29 +0100

    so many things

--- src/kernel/typer.ml
+++ src/kernel/typer.ml
@@ -25,13 +25,13 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
             | _ -> false
           ) constants
         ) with
-          | Some (_ , typ) -> typ
+          | Some (_ , typ) -> typ.desc
           | None -> raise Unknown_ident
         end
     | EVar x ->
         begin match in_context_opt x ctx
         with
-          | Some typ -> typ
+          | Some typ -> typ.desc
           | _ -> raise Unknown_ident
         end
     | ELam ((x , x_typ) , exp_of_x) ->
@@ -39,14 +39,14 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
           | Some _ -> 
               let z = get_unused_ident idl in
               let exp_of_z = rename idl x z exp_of_x in
-              compute_type_of_term ctx idl (ELam ((z , x_typ) , exp_of_z))
+              (compute_type_of_term ctx idl { desc = ELam ((z , x_typ) , exp_of_z) ; loc = term.loc }).desc
           | None ->
             let (ctx' , idl') = 
               if x = "_" then (ctx , idl) 
               else (((x , x_typ) :: ctx) , (x :: idl)) 
             in
             begin match
-                compute_type_of_term ctx idl x_typ ,
+                (compute_type_of_term ctx idl x_typ).desc ,
                 compute_type_of_term ctx' idl' exp_of_x
               with
                 | EConst "Type" , typ_of_exp_of_x ->
@@ -56,13 +56,13 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
         end
     | EApp (exp1 , exp2) ->
         begin match
-          beta_reduce idl (compute_type_of_term ctx idl exp1) ,
+          (beta_reduce idl (compute_type_of_term ctx idl exp1)).desc ,
           beta_reduce idl (compute_type_of_term ctx idl exp2)
         with
           | EPi ((id , id_typ) , typ_of_exp_of_id) , exp2_typ
             when
               alpha_compare idl id_typ exp2_typ 
-              -> substitute idl id exp2 typ_of_exp_of_id
+              -> (substitute idl id exp2 typ_of_exp_of_id).desc
           | _ -> raise Type_error
         end
     | EPi ((id , typ_a) , exp) ->
@@ -70,15 +70,15 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
           | Some _ ->
               let z = get_unused_ident idl in
               let exp_of_z = rename idl id z exp in
-              compute_type_of_term ctx idl (EPi ((z , typ_a) , exp_of_z))
+              (compute_type_of_term ctx idl { desc = EPi ((z , typ_a) , exp_of_z) ; loc = term.loc }).desc
           | None ->
               let (ctx' , idl') = 
                 if id = "_" then (ctx , idl) 
                 else (((id , typ_a) :: ctx) , (id :: idl)) 
               in
               match
-                beta_reduce idl (compute_type_of_term ctx idl typ_a) ,
-                beta_reduce idl (compute_type_of_term ctx' idl' exp)
+                (beta_reduce idl (compute_type_of_term ctx idl typ_a)).desc ,
+                (beta_reduce idl (compute_type_of_term ctx' idl' exp)).desc
               with
                 | EConst "Type" , 
                   EConst "Type" ->
@@ -87,14 +87,15 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
         end
     | EPair ((term1 , term2) , Some typ) ->
         let typ' = beta_reduce idl typ in
-        begin match typ' with
+        begin match typ'.desc with
           | ELam ((id , typ_a) , exp) ->
             begin match in_context_opt id ctx with
               | Some _ ->
                   let z = get_unused_ident idl in
                   let exp_of_z = rename idl id z exp in
-                  compute_type_of_term ctx idl 
-                    (EPair ((term1 , term2) , Some (ELam ((z , typ_a) , exp_of_z))))
+                  (compute_type_of_term ctx idl 
+                    { desc = EPair ((term1 , term2) , Some { desc = ELam ((z , typ_a) , exp_of_z) ; loc = term.loc })
+                    ; loc = term.loc }).desc
               | None ->
                   let (ctx' , idl') = 
                     if id = "_" then (ctx , idl) 
@@ -102,8 +103,8 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
                   in
                   match
                     beta_reduce idl (compute_type_of_term ctx idl term1) ,
-                    beta_reduce idl' 
-                      (compute_type_of_term ctx' idl' exp) ,
+                    (beta_reduce idl' 
+                      (compute_type_of_term ctx' idl' exp)).desc ,
                     beta_reduce idl (compute_type_of_term ctx idl term2)
                   with
                     | typ_a' , EConst "Type" , exp'
@@ -116,8 +117,8 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
                       -> ESigma ((id , typ_a) , exp)
                     | _ -> raise Type_error
             end
-          | exp ->
-            begin match beta_reduce idl (compute_type_of_term ctx idl exp) with
+          | _ ->
+            begin match (beta_reduce idl (compute_type_of_term ctx idl term)).desc with
               | EPi ((_ , typ_a) , _) ->
                   begin match beta_reduce idl (compute_type_of_term ctx idl term1) with
                     | typ when alpha_compare idl typ typ_a -> raise Unable_to_infer_type
@@ -128,13 +129,13 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
         end
     | EPair _ -> raise Unable_to_infer_type
     | EFst term ->
-        begin match compute_type_of_term ctx idl term with
-          | ESigma ((_ , typ_a) , _) -> typ_a
+        begin match (compute_type_of_term ctx idl term).desc with
+          | ESigma ((_ , typ_a) , _) -> typ_a.desc
           | _ -> raise Type_error
         end
     | ESnd term ->
-        begin match compute_type_of_term ctx idl term with
-          | ESigma ((id , _) , exp) -> substitute idl id (EFst term) exp
+        begin match (compute_type_of_term ctx idl term).desc with
+          | ESigma ((id , _) , exp) -> (substitute idl id { desc = EFst term ; loc = term.loc } exp).desc
           | _ -> raise Type_error
         end
     | ESigma ((id , typ_a) , exp) ->
@@ -142,16 +143,16 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
             | Some _ ->
                 let z = get_unused_ident idl in
                 let exp_of_z = rename idl id z exp in
-                compute_type_of_term ctx idl (ESigma ((z , typ_a) , exp_of_z))
+                (compute_type_of_term ctx idl { desc = ESigma ((z , typ_a) , exp_of_z) ; loc = term.loc }).desc
             | None ->
                 let (ctx' , idl') = 
                   if id = "_" then (ctx , idl) 
                   else (((id , typ_a) :: ctx) , (id :: idl)) 
                 in
                 match
-                  beta_reduce idl (compute_type_of_term ctx idl typ_a) ,
-                  beta_reduce idl' 
-                    (compute_type_of_term ctx' idl' exp)
+                  (beta_reduce idl (compute_type_of_term ctx idl typ_a)).desc ,
+                  (beta_reduce idl' 
+                    (compute_type_of_term ctx' idl' exp)).desc
                 with
                   | EConst "Type" , EConst "Type" -> EConst "Type"
                   | _ -> raise Type_error
@@ -160,14 +161,14 @@ let rec compute_type_of_term : context -> ident list -> expr -> expr
         if alpha_compare idl 
           (beta_reduce idl (compute_type_of_term ctx idl term))
           (beta_reduce idl exp)
-          then exp
+          then exp.desc
         else raise Type_error
   in
   if !Config.verbose then begin
     Format.eprintf "type of %a is %a\n"
       Astprinter.pp_expr term
-      Astprinter.pp_expr desc
+      Astprinter.pp_expr { desc = desc ; loc = Location.none }
   end;
   { desc = desc
-  ; loc = exp.loc
+  ; loc = term.loc
   }
