ba447be507ec49c2e384aafc766df5e342f796ff
Samuel VIVIEN <svivien@Samuels-MBP.lan>
2022-03-05 22:51:48 +0100

    implemented betared in constants

--- src/frontend/cstGenerator.ml
+++ src/frontend/cstGenerator.ml
@@ -33,15 +33,52 @@ let rec pp_expr fmt e = match e.desc with
   | EConst id ->
     Format.fprintf fmt "mk_loc (EConst \"%s\")" id
 
-let rec pp_item fmt (id, e) =
+let rec pp_cst fmt (id, e) =
   Format.fprintf fmt "(\"%s\", %a)" id pp_expr e
 
-let rec pp_list fmt = function
+let rec pp_list pp_item fmt = function
   | [] -> ()
   | h::tl ->
     Format.fprintf fmt "\t%a;\n%a"
       pp_item h
       pp_list tl;;
+
+let rec pp_in_expr fmt e = match e.desc with
+  | EVar n when String.starts_with ~prefix:"'" n -> Format.fprintf fmt "%s" (String.sub n 1 (String.length n - 1))
+  | EVar n -> Format.fprintf fmt "{desc = EVar \"%s\"; _ }" n
+  | EPi ((id, p1), p2) ->
+    Format.fprintf fmt "{ desc = EPi ((\"%s\", %a), %a); _ }"
+      id pp_expr p1 pp_expr p2
+  | ESigma ((id, p1), p2) ->
+    Format.fprintf fmt "{ desc = ESigma ((\"%s\", %a), %a); _ }"
+      id pp_expr p1 pp_expr p2
+  | ELam ((id, p1), p2) ->
+    Format.fprintf fmt "{ desc = ELam ((\"%s\", %a), %a); _ }"
+      id pp_expr p1 pp_expr p2
+  | ETaggedExpr (p1, p2) ->
+    Format.fprintf fmt "{ desc = ETaggedExpr (%a, %a); _ }"
+      pp_expr p1 pp_expr p2
+  | EPair ((p1, p2), None) ->
+    Format.fprintf fmt "{ desc = EPair ((%a, %a), None); _ }"
+      pp_expr p1 pp_expr p2
+  | EPair ((p1, p2), Some p3) ->
+    Format.fprintf fmt "{ desc = EPair ((%a, %a), Some (%a)); _ }"
+      pp_expr p1 pp_expr p2 pp_expr p3
+  | EFst p1 ->
+    Format.fprintf fmt "{ desc = EFst (%a); _ }"
+      pp_expr p1
+  | ESnd p1 ->
+    Format.fprintf fmt "{ desc = ESnd (%a); _ }"
+      pp_expr p1
+  | EApp (p1, p2) ->
+    Format.fprintf fmt "{ desc = EApp (%a, %a); _ }"
+      pp_expr p1 pp_expr p2
+  | EConst id ->
+    Format.fprintf fmt "{ desc = EConst \"%s\"; _ }" id
+
+
+let pp_bred fmt (a, b) =
+  Format.fprintf fmt "function | %a -> Some (%a) | _ -> None" pp_in_expr a pp_out_expr b
 
 
 let fdesc = open_in "kernel/primitives.knl" in
@@ -59,7 +96,10 @@ let mk_loc e = {desc = e; loc = none}
 
 let constants: context =
 [
-%a
-]
-" pp_list cst
+%a]
+
+let beta_red: Ast.expr -> Ast.expr option =
+[
+%a]
+" (pp_list pp_cst) cst (pp_list pp_bred) beta_red
 in close_out fdesc
